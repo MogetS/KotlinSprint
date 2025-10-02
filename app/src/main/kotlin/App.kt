343b49ad24c4e9d62947eeb3d1e990bb4bf9d489
astahov.sergei <ast220489@gmail.com>
2025-10-02 09:40:12 +0400

    KS-1-1 variables

--- app/src/main/kotlin/App.kt
+++ app/src/main/kotlin/App.kt
@@ -1,7 +1,5 @@
 package org.example.app
 
 fun main() {
-    val orders = 75
-    val congratulationMessage = "Спасибо что сделали покупку в нашем магазине!!!"
 
 }
--- app/src/main/kotlin/lesson_1/lesson1_task1.kt
+++ app/src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -1,6 +1,7 @@
 package org.example.app.lesson_1
 
 fun main(){
-    println("Тестовое регение к уроку 1 задаче 1")
-    println("Какие-то доработки")
+    val orders = 75
+    val congratulationMessage = "Спасибо что сделали покупку в нашем магазине!!!"
+
 }
