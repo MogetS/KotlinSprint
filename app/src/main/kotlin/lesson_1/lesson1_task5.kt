de38d992b75d7422b564712297b64e5b417ff905
astahov.sergei <ast220489@gmail.com>
2025-10-03 10:55:01 +0400

    KS-1-5 add const secondToMinute

--- app/src/main/kotlin/lesson_1/lesson1_task5.kt
+++ app/src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,9 +1,11 @@
 package org.example.app.lesson_1
 
 fun main() {
+
+    val secondInMinute = 60
     val seconds = 6480
-    var secondToTime = seconds % 60
-    var minutes: Int = (seconds / 60) % 60
+    var secondToTime = seconds % secondInMinute
+    var minutes: Int = (seconds / secondInMinute) % secondInMinute
     var hours: Int = seconds / 3600
 
     println(String.format("%02d:%02d:%02d", hours, minutes, secondToTime))
