a474689191996dc9d8a427fc3007e7f1623ef9d0
astahov.sergei <ast220489@gmail.com>
2025-10-05 20:04:57 +0400

    KS-1 update 4 and 5 task

--- app/src/main/kotlin/lesson_1/lesson1_task5.kt
+++ app/src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,17 +1,14 @@
 package org.example.app.lesson_1
 
+const val secondInMinute = 60
+const val secondInHour = 3600
 fun main() {
-    val length: Long = 40_868_600_000L
-    val age: Byte = 27
-    val partOfDay: Float = 0.075F
-    val seconds: Short = 6480
-    val partOfYear: Double = 2.0547945205479453E-4
-    val apogee: Int = 327000
 
-    println("Расстояние $length")
-    println("Возраст $age")
-    println("Часть дня $partOfDay")
-    println("Секунды $seconds")
-    println("Часть года $partOfYear")
-    println("Апогей орбиты $apogee")
+
+    val seconds = 6480
+    var secondToTime = seconds % secondInMinute
+    var minutes: Int = (seconds / secondInMinute) % secondInMinute
+    var hours: Int = seconds / secondInHour
+
+    println(String.format("%02d:%02d:%02d", hours, minutes, secondToTime))
 }
