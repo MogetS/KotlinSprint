a064e08856cc8e4c6f6b89967826f84fe7c3cc7d
astahov.sergei <ast220489@gmail.com>
2025-10-06 16:54:00 +0400

    KS-2-3 modify const name

--- app/src/main/kotlin/lesson_2/lesson2_task3.kt
+++ app/src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,18 +1,18 @@
 package org.example.app.lesson_2
 
 
-const val minuteInHour = 60
+const val MINUTE_IN_HOUR = 60
 fun main() {
     val departureTimeHour = 9                            // количество часов отправления с сервера
     val departureTimeMin = 39                            // количество минут отправления с сервера
 
 
-    val departureTime = departureTimeHour * minuteInHour + departureTimeMin // время отпрвления с сервера в минутах
+    val departureTime = departureTimeHour * MINUTE_IN_HOUR + departureTimeMin // время отпрвления с сервера в минутах
     val travelTime = 457                                 // время в пути в минутах
     val arrivalTime = departureTime + travelTime  // время прибытия в минутах
 
-    var minutes: Int = (arrivalTime / minuteInHour) % minuteInHour
-    var hours: Int = arrivalTime / minuteInHour
+    var minutes: Int = (arrivalTime / MINUTE_IN_HOUR) % MINUTE_IN_HOUR
+    var hours: Int = arrivalTime / MINUTE_IN_HOUR
 
     println("Время прибытия: " + String.format("%02d:%02d", hours, minutes))  // перобразовываем переменные  minutes, hours в  формат времени и выводим в консоль время прибытия
 
