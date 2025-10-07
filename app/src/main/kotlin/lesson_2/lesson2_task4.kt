200e638cae2ceb843c47530ecfd047b1e12efe7d
astahov.sergei <ast220489@gmail.com>
2025-10-07 10:10:11 +0400

    KS-2-4 code formatting

--- app/src/main/kotlin/lesson_2/lesson2_task4.kt
+++ app/src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,13 +1,15 @@
 package org.example.app.lesson_2
 
 
-    const val PERCENT = 100
-    const val BUFF_PERCENT = 20
-fun main(){
+const val PERCENT = 100
+const val BUFF_PERCENT = 20
+fun main() {
     val crystalOre = 7
     val ironOre = 11
-    val buffCrystalOre = (crystalOre/PERCENT.toDouble())*BUFF_PERCENT // считаем сколько дополнительной кристалической руды добудут под баффом
-    val buffIronOre = (ironOre/PERCENT.toDouble())*BUFF_PERCENT       // считаем сколько дополнительной железной руды добудут под баффом
+    val buffCrystalOre =
+        (crystalOre / PERCENT.toDouble()) * BUFF_PERCENT // считаем сколько дополнительной кристалической руды добудут под баффом
+    val buffIronOre =
+        (ironOre / PERCENT.toDouble()) * BUFF_PERCENT       // считаем сколько дополнительной железной руды добудут под баффом
 
     println("Количество кристалической руды добытой без бафа: $crystalOre")
     println("Количество железной руды добытой без бафа: $ironOre")
