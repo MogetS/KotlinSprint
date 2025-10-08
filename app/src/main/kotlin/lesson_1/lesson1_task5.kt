28097cd667ee806988dfffa0223a1c0bfbd83f21
astahov.sergei <ast220489@gmail.com>
2025-10-08 09:32:58 +0400

    KS-1-5 code formatting

--- app/src/main/kotlin/lesson_1/lesson1_task5.kt
+++ app/src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,14 +1,14 @@
 package org.example.app.lesson_1
 
-const val secondInMinute = 60
-const val secondInHour = 3600
+const val SECOND_IN_MINUTE = 60
+const val SECOND_IN_HOUR = 3600
 fun main() {
 
 
     val seconds = 6480
-    var secondToTime = seconds % secondInMinute
-    var minutes: Int = (seconds / secondInMinute) % secondInMinute
-    var hours: Int = seconds / secondInHour
+    var secondToTime = seconds % SECOND_IN_MINUTE
+    var minutes: Int = (seconds / SECOND_IN_MINUTE) % SECOND_IN_MINUTE
+    var hours: Int = seconds / SECOND_IN_HOUR
 
     println(String.format("%02d:%02d:%02d", hours, minutes, secondToTime))
 }
